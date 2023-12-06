ae83d72251b2fd494d99b26001f86f8d32ca49a0
Jake Gillow <jake.gillow@gmail.com>
2023-12-06 10:35:41 -0700

    finished, burning through leetcode 75

--- 1071.greatest_common_divisor_of_strings/greatest_common_divisor_of_strings.py
+++ 1071.greatest_common_divisor_of_strings/greatest_common_divisor_of_strings.py
@@ -1,18 +1,18 @@
 class Solution: 
     def gcdOfStrings(self, str1 : str, str2 : str) -> str:
         result : str = str1
-        biggerStr : str = str2
-        count : int = 0
+        count1 : int = 0
+        count2 : int = 0
         if len(str1) > len(str2):
             result = str2
-            biggerStr = str1
         
         while len(result) > 0:
-            count = biggerStr.count(result)
-            if count == 0:
+            count1 = str1.count(result)
+            count2 = str2.count(result)
+            if count1 == 0 or count2 == 0:
                 result = result[slice(1,len(result))]
                 continue
-            if len(biggerStr) / len(result) == count: #probably need to look at originals. 
+            if len(str1) / len(result) == count1 and len(str2) / len(result) == count2: #probably need to look at originals. 
                 return result
             else:
                 result = result[slice(1,len(result))]

142057a5f81c8aa4572d2b9950918d519a51e166
Jake Gillow <jake.gillow@gmail.com>
2023-12-18 11:34:58 -0700

    solved after some issues. spagetti code

--- 605.can_place_flowers/can_place_flowers.py
+++ 605.can_place_flowers/can_place_flowers.py
@@ -3,14 +3,27 @@ class Solution:
     def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
         if n == 0:
             return True
-        # if n == 1 and flowerbed.count(1) == 0 and len(flowerbed) == 2:
-        #     return True
         prior : bool = False
         after : bool = False
         current : bool = False
+        
+        if len(flowerbed) == 1 and n == 1 and flowerbed[0] == 0:
+            return True
 
-        #chop off the outers so you dont go out of index. 
-        for i in range(1,len(flowerbed) - 1):
+        for i in range(len(flowerbed)):
+            #first and end cases
+            if i == 0 or i == len(flowerbed) - 1:
+                if i == 0 and flowerbed[i] == 0 and flowerbed[i+1] == 0:
+                    n -= 1
+                    flowerbed[i] = 1
+                if i == len(flowerbed) - 1 and flowerbed[i-1] == 0 and flowerbed[i] == 0:
+                    n -= 1
+                    flowerbed[i] = 1
+                if n == 0:
+                    return True
+                continue
+
+            #inner cases
             if flowerbed[i-1] == 0:
                 prior = True
             else:
@@ -23,26 +36,23 @@ class Solution:
                 after = True
             else:
                 after = False
-
-            if prior and current and after == 0:
+                
+            if prior and current and after == True:
                 n -= 1
                 flowerbed[i] = 1
             if n == 0:
                 return True
         return False
-        
-        # for i in range(len(flowerbed)):
-        #     #address each case a flower could be planted
-        #     #space must be empty, not be the first space or last, and have empty adjacent spots. decriment n and add in a flower
-        #     if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
-        #         flowerbed[i] = 1
-        #         n -= 1
-        #         if n == 0:
-        #             return True
-        # return False
     
 solution : Solution = Solution()
 print(solution.canPlaceFlowers([1,0,0,0,0,0,1],2))#T
-# print(solution.canPlaceFlowers([1,0,0,0,1],2))#F
-# print(solution.canPlaceFlowers([1,0,0,0,0,1],2))#F
+print(solution.canPlaceFlowers([1,0,0,0,1],2))#F
+print(solution.canPlaceFlowers([1,0,0,0,0,1],2))#F
 print(solution.canPlaceFlowers([0,0],1))#F
+print(solution.canPlaceFlowers([0,0,1,0,1],1))#T
+print(solution.canPlaceFlowers([1,0,0,0,1,0,0],2))#T
+print(solution.canPlaceFlowers([0],1))#T
+print(solution.canPlaceFlowers([1,0,0,0],1))#T
+print(solution.canPlaceFlowers([0,0],1))#T
+print(solution.canPlaceFlowers([0,0,0,0],3))#F
+print(solution.canPlaceFlowers([0,0,0],2))#T

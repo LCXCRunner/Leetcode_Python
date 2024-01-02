a6aa7a1d703ee833ce16fed7f357c21e34efd03c
Jake Gillow <jake.gillow@gmail.com>
2024-01-01 22:27:33 -0700

    outlined problem of the day

---
+++ 2024/2610.convert_an_array_into_a_2D_array_with_conditions/convert_an_array_into_a_2D_array_with_conditions.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findMatrix(self, nums: list[int]) -> list[list[int]]:
+        print("hello world")
+
+solution : Solution = Solution()
+print(solution.findMatrix([1,3,4,1,2,3,1]))#[[1,3,4,2],[1,3],[1]]
+print(solution.findMatrix([1,2,3,4]))#[1,2,3,4]

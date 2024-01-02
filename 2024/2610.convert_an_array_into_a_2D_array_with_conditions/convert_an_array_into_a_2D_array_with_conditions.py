bf2a383331254b4f09186859efe8279d71cb4403
Jake Gillow <jake.gillow@gmail.com>
2024-01-02 08:10:31 -0700

    day 2 of january done.

--- 2024/2610.convert_an_array_into_a_2D_array_with_conditions/convert_an_array_into_a_2D_array_with_conditions.py
+++ 2024/2610.convert_an_array_into_a_2D_array_with_conditions/convert_an_array_into_a_2D_array_with_conditions.py
@@ -1,6 +1,28 @@
 class Solution:
     def findMatrix(self, nums: list[int]) -> list[list[int]]:
-        print("hello world")
+        result : list[list[int]] = [[]]
+        nums.sort()
+        sortedNums : list = nums
+        slotFound : bool = False
+
+        for i in sortedNums:
+            slotFound = False
+            iteratorSlot : int = 0
+            while slotFound == False:
+                if i in result[iteratorSlot]:
+                    if iteratorSlot == len(result) - 1:
+                        result.append([])
+                        iteratorSlot += 1
+                        result[iteratorSlot].append(i)
+                        slotFound = True
+                    else:
+                        iteratorSlot += 1
+                        continue
+                else:
+                    result[iteratorSlot].append(i)
+                    slotFound = True
+        return result
+
 
 solution : Solution = Solution()
 print(solution.findMatrix([1,3,4,1,2,3,1]))#[[1,3,4,2],[1,3],[1]]

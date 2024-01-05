d3e5b5602532ad5c890b4f66f98f984423d3e4f6
Jake Gillow <jake.gillow@gmail.com>
2024-01-05 10:11:53 -0700

    just a combination type, daily done

---
+++ 2024/300.longest_increasing_subsequence/longest_increasing_subsequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLIS(self, nums: list[int]) -> int:
+        if not nums:
+            return 0
+        
+        n = len(nums)
+        dp = [1] * n
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return max(dp)
+
+solution : Solution = Solution()
+print(solution.lengthOfLIS([10,9,2,5,3,7,101,18]))#4
+print(solution.lengthOfLIS([0,1,0,3,2,3]))#4
+print(solution.lengthOfLIS([7,7,7,7,7,7,7]))#1

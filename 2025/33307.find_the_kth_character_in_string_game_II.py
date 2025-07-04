27ca2f48ab3d65f48572de72f3c98f63044790d8
Jake Gillow <jake.gillow@gmail.com>
2025-07-04 17:20:15 -0600

    kinda treated it like a binary seach problem. done

--- 2025/33307.find_the_kth_character_in_string_game_II.py
+++ 2025/33307.find_the_kth_character_in_string_game_II.py
@@ -1,16 +1,21 @@
 class Solution:
     def kthCharacter(self, k: int, operations: list[int]) -> str:
-       word : list = ['a']
-       tempWord : list = []
-       for op in operations:
-            if op == 0:
-                word = word + word
+       # Calculate the length after each operation, we know it doubles each time regardless
+        lengths = [1]
+        for i in range(len(operations)):
+            lengths.append(2**(i+1))
+        # Work backwards to find the character at position k
+        char = 'a'
+        for i in range(len(operations)-1, -1, -1):
+            if operations[i] == 0:
+                if k > lengths[i]:
+                    k -= lengths[i]
             else:
-                for i in range(len(word)):
-                    tempWord = tempWord + [self.advanceCharacter(word[i])]
-                word = word + tempWord
-                tempWord = []
-       return word[k-1]
+                if k > lengths[i]:
+                    k -= lengths[i]
+                    # Advance the character for this operation
+                    char = self.advanceCharacter(char)
+        return char
     
     def advanceCharacter(self, c: str) -> str:
         # use ascii table - 97 is 'a', 98 is 'b', ..., 122 is 'z'
@@ -20,7 +25,7 @@ class Solution:
             return chr(ord(c) + 1)
     
 solution : Solution = Solution()
-print(solution.kthCharacter(5, [0,0,0])) # Output: a
+# print(solution.kthCharacter(5, [0,0,0])) # Output: a
 print(solution.kthCharacter(10, [0,1,0,1])) # Output: b
 print(solution.kthCharacter(4, [1,1])) # Output: c
 print(solution.kthCharacter(9499052, [0,0,0,1,1,0,1,1,1,0,1,0,1,1,1,0,0,1,1,1,0,0,0,1,1])) # Output: h

b3b3f1606e6da67bc8544013e2095dc5379d6e44
Jake Gillow <jgillow@asu.edu>
2025-07-06 11:47:12 -0600

    improved a bit, still not enough.

--- 2025/1865.finding_pairs_with_a_certain_sum.py
+++ 2025/1865.finding_pairs_with_a_certain_sum.py
@@ -9,11 +9,32 @@ class FindSumPairs:
 
     def count(self, tot: int) -> int:
         result : int = 0
-        for i in range(len(self.nums1)):
-            for j in range(len(self.nums2)):
-                if self.nums1[i] + self.nums2[j] == tot:
+        self.nums1.sort()
+        self.nums2.sort()
+
+        for i in range(len(self.nums2)):
+            for j in range(len(self.nums1) - 1, -1, -1):
+                sum : int = self.nums2[i] + self.nums1[j]
+                if sum < tot:
+                    break # need to move nums2 up
+                elif sum > tot:
+                    continue
+                else:
                     result += 1
         return result
+    
+solution : FindSumPairs = FindSumPairs([1,1,2,2,2,3], [1,4,5,2,5,4])
+print(solution.count(7)) #8
+    
+    # notes:
+    # 1 <= nums1.length <= 1000
+    # 1 <= nums2.length <= 10^5
+    # 1 <= nums1[i] <= 10^9
+    # 1 <= nums2[i] <= 10^5
+    # 0 <= index < nums2.length
+    # 1 <= val <= 10^5
+    # 1 <= tot <= 10^9
+    # At most 1000 calls are made to add and count each.
 
 # Your FindSumPairs object will be instantiated and called as such:
 # obj = FindSumPairs(nums1, nums2)

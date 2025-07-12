f694d597f4202d905ad7771dd43a905c6455c9c2
Jake Gillow <jake.gillow@gmail.com>
2025-07-11 18:33:24 -0600

    ruby is hungry so we have to go. I'll solve it later.

---
+++ 2025/735.asteroid_collision.py
@@ -0,0 +1,8 @@
+class Solution:
+    def asteroidCollision(self, asteroids: list[int]) -> list[int]:
+        print('hello world')
+
+solution : Solution = Solution()
+print(solution.asteroidCollision([5,10,-5])) # [5,10]
+print(solution.asteroidCollision([8,-8])) # []
+print(solution.asteroidCollision([10,2,-5])) # [10]

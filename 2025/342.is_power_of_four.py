a255cc61829dac13d99fb92a318a545614cf20f7
Jake Gillow <jake.gillow@gmail.com>
2025-08-14 19:59:49 -0600

    daily leetcode on the train

---
+++ 2025/342.is_power_of_four.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n == 1:
+            return True
+        if n <= 0:
+            return False
+        if n % 4 != 0:
+            return False
+        while n > 4:
+            n = n / 4
+            if n % 4 != 0:
+                return False
+        return True
+
+solution : Solution = Solution()
+print(solution.isPowerOfFour(16))  # True
+print(solution.isPowerOfFour(5))   # False
+print(solution.isPowerOfFour(1))   # True
+print(solution.isPowerOfFour(256)) # True
+print(solution.isPowerOfFour(257)) # False
+print(solution.isPowerOfFour(0)) # False
+print(solution.isPowerOfFour(8)) # False
+print(solution.isPowerOfFour(-2147483648)) # False

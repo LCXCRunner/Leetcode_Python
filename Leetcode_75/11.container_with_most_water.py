022ea1069ffe4294e28bde72be4b2b39058253b9
Jake Gillow <jake.gillow@gmail.com>
2025-11-17 10:05:03 -0700

    caught up from the previous day with double pointers.

--- Leetcode_75/11.container_with_most_water.py
+++ Leetcode_75/11.container_with_most_water.py
@@ -1,6 +1,18 @@
 class Solution:
     def maxArea(self, height: list[int]) -> int:
-        print('hell world')
+        n : int = len(height)
+        left : int = 0
+        right : int = n - 1
+        maxArea : int = 0
+
+        while left < right:
+            area : int = abs(right - left) * min(height[left], height[right])
+            maxArea = max(maxArea, area)
+            if height[left] > height[right]:
+                right -= 1
+            else:
+                left += 1
+        return maxArea
 
 solution : Solution = Solution()
 print(solution.maxArea([1,8,6,2,5,4,8,3,7])) # 49

87b3fd3c8be8b6cf68ad0adad7008e46fed4b804
Jake Gillow <jake.gillow@gmail.com>
2025-11-16 18:56:11 -0700

    setup medium two pointer for tomorrow

---
+++ Leetcode_75/11.container_with_most_water.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        print('hell world')
+
+solution : Solution = Solution()
+print(solution.maxArea([1,8,6,2,5,4,8,3,7])) # 49
+print(solution.maxArea([1,1])) # 1

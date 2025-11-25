76906e7a2c1e838de634d470d6df19a6a57bcd67
Jake Gillow <jake.gillow@gmail.com>
2025-11-24 18:22:44 -0700

    working on time limit for 2390. Works, just not efficient enough just yet

---
+++ Leetcode_75/2390.removing_stars_from_a_string.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class Solution:
+    def removeStars(self, s: str) -> str:
+        result : deque = deque()
+        stack : deque = deque(s)
+        stack.reverse()
+        
+        while len(stack) != 0:
+            if stack[0] != "*":
+                result.append(stack.pop())
+            else:
+                result.pop()
+                stack.pop()
+        return "".join(result)
+                
+
+
+solution : Solution = Solution()
+print(solution.removeStars("leet**cod*e")) # "lecoe"
+print(solution.removeStars("erase*****")) # ""

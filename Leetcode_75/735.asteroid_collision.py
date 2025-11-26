e1bda4dfafe8890a41000c677552f39629f5dce4
Jake Gillow <jake.gillow@gmail.com>
2025-11-26 10:43:31 -0700

    done with stacks, moving on to queues

--- Leetcode_75/735.asteroid_collision.py
+++ Leetcode_75/735.asteroid_collision.py
@@ -1,6 +1,19 @@
+from collections import deque
+
 class Solution:
     def asteroidCollision(self, asteroids: list[int]) -> list[int]:
-        print('hello world')
+        stack : deque = deque()
+        for asteroid in asteroids:
+            while stack and asteroid < 0 < stack[-1]:
+                if stack[-1] < -asteroid:
+                    stack.pop()
+                    continue
+                elif stack[-1] == -asteroid:
+                    stack.pop()
+                break
+            else:
+                stack.append(asteroid)
+        return list(stack)
 
 solution : Solution = Solution()
 print(solution.asteroidCollision([5,10,-5])) # [5,10]

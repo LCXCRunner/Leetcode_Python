79fa72235f5a353100b6ed50615fc9b9762f642d
Jake Gillow <jake.gillow@gmail.com>
2025-12-05 17:38:29 -0700

    stuck on 547. Coming back to it later.

--- Leetcode_75/547.number_of_provinces.py
+++ Leetcode_75/547.number_of_provinces.py
@@ -1,7 +1,29 @@
+from collections import deque
 class Solution:
     def findCircleNum(self, isConnected: list[list[int]]) -> int:
-        print('Hello World')
+        provinces : list[set] = [{0}]
+        stack : deque = deque([0])  
+        
+        visited : set = set()
+        visited.add(0)
+        n : int = len(isConnected)
+        while stack:
+            node : int = stack.pop()
+            for neighbor in range(n):
+                if isConnected[node][neighbor] == 1 and neighbor not in visited:
+                    visited.add(neighbor)
+                    stack.append(neighbor)
+                    for province in provinces:
+                        if node in province:
+                            province.add(neighbor)
+                            break
+                    else:
+                        provinces.append({neighbor})
+
+        return len(provinces)
+
 
 solution : Solution = Solution()
 print(solution.findCircleNum([[1,1,0],[1,1,0],[0,0,1]])) # 2
 print(solution.findCircleNum([[1,0,0],[0,1,0],[0,0,1]])) # 3
+print(solution.findCircleNum([[1,0,0,1],[0,1,1,0],[0,1,1,1],[1,0,1,1]])) # 1

076ed8def8833e12bf6546719281a9e48932b812
Jake Gillow <jake.gillow@gmail.com>
2025-12-08 16:51:01 -0700

    finally got through the graph problem. more tomorrow.

---
+++ Leetcode_75/1466.reorder_routes_to_make_all_paths_lead_to_the_city_zero.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+class Solution:
+    def minReorder(self, n: int, connections: list[list[int]]) -> int:
+        # construct the graph as well as the list of roads
+        # graph:
+        # key: city --- values: [roads, that, connect, to, city]
+        #
+        # roads:
+        # a set with number pairs: {(0, 1), (4, 0), ...}
+        roads : set = set()
+        graph : defaultdict[list] = defaultdict(list)
+        for x, y in connections:
+            graph[x].append(y)
+            graph[y].append(x)
+            roads.add((x,y))
+
+        def dfs(node):
+            ans = 0
+            # will iterate through each value of the graph. 
+            for neighbor in graph[node]:
+                if neighbor not in seen:
+                    # basically looking to see if the road goes in the direction of the capital. 
+                    if (node, neighbor) in roads: # This is the edge
+                        ans += 1
+                    seen.add(neighbor)
+                    ans += dfs(neighbor)
+            return ans
+
+        seen = {0}
+        return dfs(0)
+    
+solution : Solution = Solution()
+print(solution.minReorder(n = 6, connections = [[0,1],[1,3],[2,3],[4,0],[4,5]])) # 3
+print(solution.minReorder(n = 5, connections = [[1,0],[1,2],[3,2],[3,4]])) # 2
+print(solution.minReorder(n = 3, connections = [[1,0],[2,0]])) # 0

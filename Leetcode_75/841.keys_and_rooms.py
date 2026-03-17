4af1a480b8fcc7fcbdd3b748047a02f6b14bd964
Jake Gillow <jake.gillow@gmail.com>
2026-03-17 14:49:55 -0600

    pushing just for adding new test cases

--- Leetcode_75/841.keys_and_rooms.py
+++ Leetcode_75/841.keys_and_rooms.py
@@ -19,10 +19,14 @@ class Solution:
         if len(keyRing) == len(rooms):
             return True
         return False
-            
-
 
 solution : Solution = Solution()
 
 print(solution.canVisitAllRooms([[1],[2],[3],[]])) # True
 print(solution.canVisitAllRooms([[1,3],[3,0,1],[2],[0]])) # False
+print(solution.canVisitAllRooms([[2],[],[1]]))  # True
+print(solution.canVisitAllRooms([[1,3],[1,4],[2,3,4,1],[],[4,3,2]]))  # True
+print(solution.canVisitAllRooms([[1],[],[0,3],[1]]))  # False
+print(solution.canVisitAllRooms([[],[1,15,18],[16],[2,3,9,11,17,5],[15,19,8,12,14],[10,1,6],[12,9,11],[],[7],[13],[3],[16,2],[4],[18,13],[7,17],[6],[14,4],[5],[8,19],[10]]))  # False
+print(solution.canVisitAllRooms([[1],[],[0,3],[1]]))  # False
+print(solution.canVisitAllRooms([[1,3],[1,4],[2,3,4,1],[],[4,3,2]]))  # True

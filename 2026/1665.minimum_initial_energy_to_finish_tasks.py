ace14a5cad46afd87aa34aa9c14ff275b35f0c1b
Jake Gillow <jake.gillow@gmail.com>
2026-05-11 22:59:38 -0500

    test of git bug

--- 2026/1665.minimum_initial_energy_to_finish_tasks.py
+++ 2026/1665.minimum_initial_energy_to_finish_tasks.py
@@ -3,6 +3,7 @@ class Solution:
         print("hello world")
 
 print("test")
+print("test2")
 tasks = [[1, 2], [2, 4], [4, 8]]
 task2 = [[1, 2], [2, 4], [4, 8], [1, 1]]
 print(Solution().minimumEffort(tasks))
